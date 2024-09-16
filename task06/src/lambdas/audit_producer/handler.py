22a9953a499cdb692ed51d384667afdfd7ce8dce
AmitKumar Choudhary <amitkumar_choudhary@epam.com>
2024-09-16 22:28:53 +0530

    added configuration and audit table for dynamo db stream

--- task06/src/lambdas/audit_producer/handler.py
+++ task06/src/lambdas/audit_producer/handler.py
@@ -1,7 +1,12 @@
+import os
+
+import boto3
 from commons.abstract_lambda import AbstractLambda
 from commons.log_helper import get_logger
 
 _LOG = get_logger('AuditProducer-handler')
+
+dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
 
 
 class AuditProducer(AbstractLambda):
@@ -13,6 +18,9 @@ class AuditProducer(AbstractLambda):
         """
         Explain incoming event here
         """
+        config_table = dynamodb.Table(os.environ.get("trigger_table"))
+        audit_table = dynamodb.Table(os.environ.get("target_table_name"))
+
         _LOG("===============>>>>>", event)
         return 200
 
